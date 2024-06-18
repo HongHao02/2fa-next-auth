b5ba9fb16114d8ab930a1e658c4feffef5e709e0
HongHaoNP <99229574+HongHao02@users.noreply.github.com>
2024-06-18 09:17:09 +0700

    #2 two factor by email

--- actions/login.ts
+++ actions/login.ts
@@ -8,6 +8,9 @@ import { AuthError } from 'next-auth';
 import { generateVerificationToken, generateTwoFactorToken } from '@/lib/token';
 import { sendVrificationEmail, sendTwoFactorTokenEmail } from '@/lib/mail';
 import { getUserByEmail } from '@/data/user';
+import { getTwoFactorTokenByEmail } from '@/data/two-factor-token';
+import { db } from '@/lib/db';
+import { getTwoFactorConfirmationByUserId } from '@/data/two-factor-confirmation';
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
     console.log(values);
@@ -15,7 +18,7 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     if (!validatedValues.success) {
         return { error: 'Invalid fields' };
     }
-    const { email, password } = validatedValues.data;
+    const { email, password, code } = validatedValues.data;
 
     const existingUser = await getUserByEmail(email);
     if (!existingUser || !existingUser.email || !existingUser.password) {
@@ -27,10 +30,38 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
         return { success: 'Check your email to confirm login!' };
     }
     if (existingUser.isTwoFactorEnabled && existingUser.email) {
-        const twoFactorToken = await generateTwoFactorToken(existingUser.email);
-        await sendTwoFactorTokenEmail(existingUser.email, twoFactorToken.token);
+        if (code) {
+            const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
+            if (!twoFactorToken) {
+                return { error: 'Invalid code' };
+            }
+            if (twoFactorToken.token !== code) {
+                return { error: 'Your 2FA code does not match!' };
+            }
+            const hasExpired = new Date(new Date(twoFactorToken.expires)) < new Date();
+            if (hasExpired) {
+                return { error: 'Token has expired!' };
+            }
+            await db.twoFactorToken.delete({
+                where: { id: twoFactorToken.id },
+            });
+            const existingConfirmation= await getTwoFactorConfirmationByUserId(existingUser.id)
+            if(existingConfirmation){
+                await db.twoFactorConfirmation.delete({
+                    where: {id: existingConfirmation.id}
+                })
+            }
+            await db.twoFactorConfirmation.create({
+                data:{
+                    userId: existingUser.id
+                }
+            })
+        } else {
+            const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+            await sendTwoFactorTokenEmail(existingUser.email, twoFactorToken.token);
 
-        return {twoFactor: true}
+            return { twoFactor: true };
+        }
     }
 
     try {
