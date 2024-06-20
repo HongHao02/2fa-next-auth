1de41296a650b1f290dc77ce0a73c2ecfb9c5297
HongHaoNP <99229574+HongHao02@users.noreply.github.com>
2024-06-20 14:58:42 +0700

    #2 fix Suspense for /auth/error page

--- app/auth/error/page.tsx
+++ app/auth/error/page.tsx
@@ -4,12 +4,20 @@ import React, { Suspense } from 'react';
 
 import ErrorCard from '@/components/auth/error-card';
 import { useSearchParams } from 'next/navigation';
-const ErrorPage = () => {
+function ErrorWrapper() {
     const searchParams = useSearchParams();
     const error = searchParams.get('error');
+
+    return (
+        <>
+            <ErrorCard error={error}></ErrorCard>
+        </>
+    );
+}
+const ErrorPage = () => {
     return (
         <Suspense>
-            <ErrorCard error={error}></ErrorCard>
+            <ErrorWrapper></ErrorWrapper>
         </Suspense>
     );
 };
