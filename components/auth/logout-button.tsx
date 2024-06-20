5ab3ce61e7668ae2d075955b0d2f47cad469f84b
HongHaoNP <99229574+HongHao02@users.noreply.github.com>
2024-06-20 16:25:21 +0700

    #3 fix signOut redirect

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -1,14 +1,16 @@
 'use client';
 import React from 'react';
 import { signOut } from 'next-auth/react';
+import { usePathname } from 'next/navigation';
 
 interface LogoutButtonProps {
     children?: React.ReactNode;
 }
 
 const LogoutButton = ({ children }: LogoutButtonProps) => {
+    const pathname = usePathname();
     const onClick = () => {
-        signOut({ redirect: true });
+        signOut({ redirect: true, callbackUrl: pathname });
     };
     return (
         <span onClick={onClick} className="cursor-pointer">
