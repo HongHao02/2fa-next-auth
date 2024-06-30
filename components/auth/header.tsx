724cb1d1a50d95e76b82b624c2e40e8bcf2513e4
HongHao <honghaocp@gmail.com>
2024-06-30 23:45:37 +0700

    change logo for card header

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -9,7 +9,7 @@ interface HeaderProps{
 const Header = ({label}:HeaderProps) => {
   return (
     <div className='w-full flex justify-center items-center flex-col gap-y-4'>
-        <h1 className={cn("text-3xl font-semibold", poppins.className)}>🔐Auth</h1>
+        <h1 className={cn("text-3xl font-semibold", poppins.className)}>📧MAILBOX-APP</h1>
         <p className='text-muted-foreground text-sm'>{label}</p>
     </div>
   )
